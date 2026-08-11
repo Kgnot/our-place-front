3d49da859822303e4df4ae091e4e2804049a36ec
Kgnot <hricaurtem@proton.me>
2026-08-10 22:01:21 -0500

    Update environment configuration to switch API URL to localhost for local development

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
   production: false,
-  // apiUrl: 'http://localhost:8080/api/v1'
-  apiUrl: 'http://192.168.1.5:8080/api/v1', // Usa la IP que anotaste en el paso 1
+  apiUrl: 'http://localhost:8080/api/v1'
+  // apiUrl: 'http://192.168.1.5:8080/api/v1', // Usa la IP que anotaste en el paso 1
 };
